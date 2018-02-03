617077784d870fcfe64ba68cef8f41410affe553
Christian Prescott <me@christianprescott.com>
2018-02-02 20:22:00 -0700

    Add empty three.js scene

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-function component () {
-  const element = document.createElement('div')
-  element.innerText = 'Hello webpack'
-  return element
+import * as THREE from 'three'
+
+class Component {
+  constructor () {
+    const renderer = new THREE.WebGLRenderer()
+
+    const scene = new THREE.Scene()
+
+    const camera = new THREE.PerspectiveCamera(45, 1, 1, 1000)
+    scene.add(camera)
+
+    Object.assign(this, { renderer, scene, camera })
+  }
+
+  domElement () {
+    return this.renderer.domElement
+  }
+
+  start () {
+    const { renderer, scene, camera } = this
+    renderer.render(scene, camera)
+  }
 }
 
-document.body.appendChild(component())
+const component = new Component()
+component.start()
+document.body.appendChild(component.domElement())
