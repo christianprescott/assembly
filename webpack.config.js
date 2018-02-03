133a9199a2cc20a752a889685f583f58a6c1a770
Christian Prescott <me@christianprescott.com>
2018-02-02 21:07:19 -0700

    Add babel-loader, transform-class-properties plugin

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,20 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: ['transform-class-properties'],
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 }
