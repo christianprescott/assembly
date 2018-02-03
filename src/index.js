ae3c49c3dd0ac7d24edcdccb3c4a1f3606c48dad
Christian Prescott <me@christianprescott.com>
2018-02-02 21:08:29 -0700

    Add animation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import * as THREE from 'three'
 
 class Component {
+  clock = new THREE.Clock(false)
+
   constructor () {
     const renderer = new THREE.WebGLRenderer()
 
     const scene = new THREE.Scene()
 
     const camera = new THREE.PerspectiveCamera(45, 1, 1, 1000)
+    camera.position.set(0, -5, 0)
+    camera.rotation.set(Math.PI / 2, 0, 0)
     scene.add(camera)
 
-    Object.assign(this, { renderer, scene, camera })
+    const cube = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    )
+    scene.add(cube)
+
+    Object.assign(this, { renderer, scene, camera, cube })
   }
 
   domElement () {
@@ -17,6 +27,22 @@ class Component {
   }
 
   start () {
+    this.clock.start()
+    this._animate()
+  }
+
+  _animate = () => {
+    requestAnimationFrame(this._animate)
+    const dt = this.clock.getDelta()
+    this._update(dt)
+    this._render()
+  }
+
+  _update (dt) {
+    this.cube.rotation.z += 1.0 * dt
+  }
+
+  _render () {
     const { renderer, scene, camera } = this
     renderer.render(scene, camera)
   }
