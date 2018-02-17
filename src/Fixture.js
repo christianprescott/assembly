db99a34bb77baf1965416fb7b1bc4bad17b59218
Christian Prescott <me@christianprescott.com>
2018-02-17 14:33:30 -0700

    Use toon material for fixtures
    which I didn't do earlier for some reason

--- src/Fixture.js
+++ src/Fixture.js
@@ -1,11 +1,11 @@
-import { Mesh, MeshBasicMaterial } from 'three'
+import { Mesh, MeshToonMaterial } from 'three'
 
 export default class Fixture extends Mesh {
   constructor (geometry) {
     if (!geometry) throw new Error('geometry must be present')
     super(
       geometry,
-      new MeshBasicMaterial({ color: 0x808080 }),
+      new MeshToonMaterial({ color: 0x808080 }),
     )
     this.body = null
   }
