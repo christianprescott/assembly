89f16fb0c87589bc233c1ab0a3d2ed7c50c1001e
Christian Prescott <me@christianprescott.com>
2018-02-18 09:43:04 -0700

    Bundle as library 'assembly'

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import App from './App'
 import Assembly from './Assembly'
 import example from './examples/bricks.json'
 
-const parent = document.getElementById('container')
-const app = new App(parent)
-const assembly = Assembly.load(example)
-app.load(assembly)
-app.start()
+export { App, Assembly, example }
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
+    library: 'assembly',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
