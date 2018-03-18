bed800382e7a9db1412d63cc929b0f5f0459e093
Christian Prescott <me@christianprescott.com>
2018-03-17 20:16:48 -0600

    Add rotation target representation to debug

--- src/Link.js
+++ src/Link.js
@@ -1,4 +1,5 @@
 import {
+  EllipseCurve,
   Face3,
   Geometry,
   Line,
@@ -19,6 +20,12 @@ const GEO_ARROW = new Geometry().setFromPoints([
   new Vector3(0, 0.1, 0),
 ])
 GEO_ARROW.faces.push(new Face3(0, 1, 2))
+const POINT = [
+  new Vector3(0, 1, 0),
+  new Vector3(-0.2, 1, 0.2),
+  new Vector3(-0.2, 1, -0.2),
+  new Vector3(0, 1, 0),
+]
 
 export default class Link extends Object3D {
   static create (meshA, meshB) {
@@ -44,6 +51,7 @@ export default class Link extends Object3D {
     meshA.add(this)
 
     if (DEBUG) {
+      // Link indicator
       const length = this.position.length()
       this.arrowMesh = new Mesh(GEO_ARROW, MAT_ARROW)
       this.arrowMesh.position.copy(this.position).negate()
@@ -52,10 +60,23 @@ export default class Link extends Object3D {
       this.arrowMesh.lookAt(0, 0, 0)
       this.add(this.arrowMesh)
 
+      // Position target
       const lineGeo = new Geometry().setFromPoints([new Vector3(), new Vector3()])
       lineGeo.computeLineDistances()
       this.line = new Line(lineGeo, MAT_LINE)
       this.add(this.line)
+
+      // Rotation target
+      this.curve = new EllipseCurve(
+        0, 0,
+        1, 1, // xRadius, yRadius
+        0, 0, // startAngle, endAngle
+        false, // clockwise
+        Math.PI / 2, // rotation
+      )
+      const arcGeo = new Geometry().setFromPoints(this.curve.getPoints(12))
+      this.arc = new Line(arcGeo, MAT_LINE)
+      this.add(this.arc)
     }
 
     this.meshes()
@@ -70,12 +91,23 @@ export default class Link extends Object3D {
   getDistance () {
     // from meshA THREE mesh to meshB CANNON body
     const distance = this.getWorldPosition().distanceTo(this.meshB.body.position)
+    const [axis, angle] = this.meshB.body.quaternion.toAxisAngle()
     if (DEBUG) {
+      // Position distance
       this.line.geometry.vertices[1].subVectors(this.meshB.body.position, this.getWorldPosition())
       this.line.geometry.verticesNeedUpdate = true
       this.line.geometry.lineDistancesNeedUpdate = true
       this.line.geometry.computeLineDistances()
+      // Rotation distance
+      // TODO: This doesn't quite point in the direction it should... but the curve with
+      // angle is useful
+      this.curve.aEndAngle = angle
+      this.arc.geometry.setFromPoints(POINT.concat(this.curve.getPoints(12)))
+      this.arc.geometry.verticesNeedUpdate = true
+      this.arc.quaternion.setFromAxisAngle(new Vector3(0, 0, 1).cross(axis), Math.PI / 2)
+
       this.arrowMesh.material = distance < 0.05 ? MAT_ARROW_LINKED : MAT_ARROW
+      this.arc.material = angle < 0.05 ? MAT_ARROW_LINKED : MAT_ARROW
     }
     return distance
   }
