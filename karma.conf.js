e321723ee2dd90fed52d72d12c44954518ebe4ef
Christian Prescott <me@christianprescott.com>
2018-03-24 11:16:30 -0600

    Use mocha karma reporter

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,15 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
+    mochaReporter: {
+        colors: {
+            error: 'redBright',
+            info: 'white',
+            success: 'greenBright',
+            warning: 'yellowBright'
+        }
+    },
     // web server port
     port: 9876,
     // enable / disable colors in the output (reporters and logs)
