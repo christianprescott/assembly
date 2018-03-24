023d0ddbc45d08852ebfd45b58c9f166bc687bf0
Christian Prescott <me@christianprescott.com>
2018-03-24 11:08:30 -0600

    Use chai js assertions

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function(config) {
     basePath: '',
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'chai'],
     // list of files / patterns to load in the browser
     files: [
       'src/__tests__/*.test.js'
--- src/__tests__/shapeFromGeometry.test.js
+++ src/__tests__/shapeFromGeometry.test.js
@@ -2,14 +2,16 @@ import { Box, Vec3 } from 'cannon'
 import shapes from '../../test/fixtures/shapes'
 import shapeFromGeometry from '../shapeFromGeometry'
 
-const assert = require('assert')
-
 describe('shapeFromGeometry', function () {
+  function expectVec3Eq (v1, v2) {
+    expect(v1.almostEquals(v2)).to.eq(true, `expected ${v1} to equal ${v2}`)
+  }
+
   context('Box', () => {
     it('returns geometry bounding box', function () {
       const { shape } = shapeFromGeometry(shapes.boxCube)
-      assert.ok(shape instanceof Box)
-      assert.ok(shape.halfExtents.almostEquals(new Vec3(0.5, 0.5, 0.5)))
+      expect(shape).to.be.instanceOf(Box)
+      expectVec3Eq(shape.halfExtents, new Vec3(0.5, 0.5, 0.5))
     })
   })
 })
